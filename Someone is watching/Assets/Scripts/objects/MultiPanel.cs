b25cb9c29ca86d4ce37cddfffd12f268399625ce
charles0801 <wqypoiu@live.cn>
2021-04-26 21:56:52 +0800

    multipanel button fixed

--- Someone is watching/Assets/Scripts/objects/MultiPanel.cs
+++ Someone is watching/Assets/Scripts/objects/MultiPanel.cs
@@ -14,43 +14,65 @@ public class MultiPanel : MonoBehaviour
     public string State = "";
     Dictionary<int, Sprite[]> PicDics = new Dictionary<int, Sprite[]>();
 
+    private GameObject left;
+    private GameObject right;
 
-    int id;
-
+    int id = 0;
     private int currentID;
     // Start is called before the first frame update
 
+    enum category
+    {
+        medicine,
+        book,
+    }
     private void Start()
     {
         //PicDics.Add(0, medicines);
         //PicDics.Add(1, medicineBook);
+        left = transform.Find("Left").gameObject;
+        left.SetActive(false);
+        right = transform.Find("Right").gameObject;
+        currentID = 0;
     }
 
 
     void InitDic()
     {
-        PicDics[0] = medicines;
-        PicDics[1] = medicineBook;
+        PicDics[(int)category.medicine] = medicines;
+        PicDics[(int)category.book] = medicineBook;
     }
 
     public void UpdatePanel(int id)
     {
         InitDic();
-        currentID = id;
+        currentID = 0;
         Pictures = PicDics[id];
         maxNum = Pictures.Length-1;
-        m_Image.sprite = Pictures[id];
+        m_Image.sprite = Pictures[currentID];
     }
 
+    private void buttonHelper()
+    {
+        if (currentID <= 0)
+        {
+            left.SetActive(false);
+            right.SetActive(true);
+        } else if(currentID >= maxNum) {
+            left.SetActive(true);
+            right.SetActive(false);
+        } else
+        {
+            left.SetActive(true);
+            right.SetActive(true);
+        }
+    }
     public void LeftClick()
     {
-        id--;
-        if (id < 0)
-        {
-            id = maxNum;
-        }
+        currentID--;
+        buttonHelper();
         m_Image.sprite = Pictures[id];
-        if (currentID == 1)
+        if (id == 1)
         {
             Sound.Instance.PlayEffect("SoundEffect/Sound_Flip");
         }
@@ -58,13 +80,10 @@ public class MultiPanel : MonoBehaviour
 
     public void RightClick()
     {
-        id++;
-        if (id >maxNum)
-        {
-            id = 0;
-        }
-        m_Image.sprite = Pictures[id];
-        if (currentID == 1)
+        currentID++;
+        buttonHelper();
+        m_Image.sprite = Pictures[currentID];
+        if (id == 1)
         {
             Sound.Instance.PlayEffect("SoundEffect/Sound_Flip");
         }
